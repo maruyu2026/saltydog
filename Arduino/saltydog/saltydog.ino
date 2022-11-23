dbe318c092d57c03031a4e835152bca2d27bd6b3
maruyu2026 <maruyu1998@gmail.com>
2022-11-23 22:28:01 +0900

    実機のことを考え出した今日この頃

--- Arduino/saltydog/saltydog.ino
+++ Arduino/saltydog/saltydog.ino
@@ -1,59 +1,122 @@
 #include "time.h"
 #include "lpf.h"
+#include <Servo.h>
 
-// 1: debug mode 0: 実機mode
-#define DEBUG 1
+// 0:debug MODE 1:実機 TEST MODE 2:実機 本番 MODE
+#define MODE 0
 
-#ifdef DEBUG
+// pin配置
+#if MODE == 0
 const int mic_pin = A2;
 const int led_pin =  4;
-#else
-const int mic_pin =  6;
-const int led_pin =  4;
+
+#elif MODE == 1
+const int mic_pin =  A0;
+const int led_pin = 13;
+
+#elif MODE == 2
+const int mic_pin =  A0;
+const int led_pin = 13;
+const int servo_pin = 11;
+
+#endif
+
+// サーボ設定
+#if MODE == 2
+Servo servo;
+const int servo_1st_ang = 0;
+const int servo_2nd_ang = 90;
 #endif
 
 
+// class
 TimeClass wait_time(1000);
 
 LpfClass lpf1(0.6);
 LpfClass lpf2(0.6);
 
+// micの閾値
 const int siki_val = 500;
 
-void setup() {
-  Serial.begin(115200);
-  while(!Serial);
+//func
+void set_pin_mode(void);
+void set_serial(void);
+void send_data(int, int);
+void servo_move(int);
 
-  pinMode(led_pin, OUTPUT);
+
+void setup()
+{
+  set_pin_mode();
+  set_serial();
 }
 
-void loop() {
+void loop() 
+{
   int mic_val = analogRead(mic_pin);
   int mic_val_lpf = lpf1.LPF(lpf2.LPF(mic_val));
+
+  send_data(mic_val, mic_val_lpf);
+  servo_move(mic_val_lpf);
+}
+
+void set_pin_mode(void)
+{
+  pinMode(led_pin, OUTPUT);
+  
+  #if MODE == 2
+  servo.attach(servo_pin);
+  #endif
+}
+
+void set_serial(void)
+{
+  Serial.begin(115200);
+  while(!Serial);
+}
+
+void send_data(int nama, int lpf)
+{
   Serial.print(0);
   Serial.print(",");
   Serial.print(1024);
   Serial.print(",");
   Serial.print(siki_val);
   Serial.print(",");
-  Serial.print(mic_val);
+  Serial.print(nama);
   Serial.print(",");
-  Serial.print(mic_val_lpf);
+  Serial.print(lpf);
   Serial.print("\n");
-  
+}
+
+void servo_move(int val)
+{
   static int count = 0;
   switch(count)
   {
   case 0:
-    if(mic_val_lpf > siki_val){
+    if(val > siki_val)
+    {
       wait_time.time_ms_wait(false);
       count = 1;
+    }
+    else
+    {
+      #if MODE == 2
+      servo.write(servo_1st_ang);
+      #endif
     }
     break;
   
   case 1:
     digitalWrite(led_pin, HIGH);
-    if(!wait_time.time_ms_wait(true)){
+
+    #if MODE == 2
+    servo.write(servo_2nd_ang);
+    #endif
+
+    if(!wait_time.time_ms_wait(true))
+    {
       digitalWrite(led_pin, LOW);
       count = 0;
     }
